995380fd624cb5ad8d7ce4132ecbc30924a25a03
Mike Driscoll <mike@pythonlibrary.org>
2021-08-11 14:17:53 -0500

    add more code examples for chapter 6

---
+++ 06_charts/chart_styles.py
@@ -0,0 +1,44 @@
+# chart_styles.py
+
+from openpyxl import Workbook
+from openpyxl.chart import BarChart, Reference
+
+
+def main(filename, style=None):
+    workbook = Workbook()
+    sheet = workbook.active
+
+    # Add data to spreadsheet
+    data_rows = [
+        ["Book", "Kindle", "Paperback"],
+        [1, 9.99, 15.99],
+        [2, 9.99, 25.99],
+        [3, 9.99, 25.99],
+        [4, 4.99, 29.99],
+        [5, 14.99, 39.99],
+    ]
+
+    for row in data_rows:
+        sheet.append(row)
+
+    # Create the bar chart
+    bar_chart = BarChart()
+
+    data = Reference(worksheet=sheet,
+                     min_row=1,
+                     max_row=10,
+                     min_col=2,
+                     max_col=3)
+    bar_chart.add_data(data, titles_from_data=True)
+    bar_chart.title = f"Chart Style {style}"
+    sheet.add_chart(bar_chart, "E2")
+
+    if style is not None:
+        bar_chart.style = style
+
+    workbook.save(filename)
+
+
+if __name__ == "__main__":
+    for style in range(1, 4):
+        main(f"chart_style{style}.xlsx", style)
