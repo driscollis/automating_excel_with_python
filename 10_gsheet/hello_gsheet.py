623ede1ca93a2d2f62a0f6daa2e0b12f02acb6b0
Mike Driscoll <mike@pythonlibrary.org>
2021-10-07 09:27:14 -0500

    add Google Sheets examples

---
+++ 10_gsheet/hello_gsheet.py
@@ -0,0 +1,32 @@
+# hello_gsheet.py
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+def authenticate(credentials):
+    scope = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive",
+    ]
+    creds = ServiceAccountCredentials.from_json_keyfile_name(credentials, scope)
+    client = gspread.authorize(creds)
+    return client
+
+
+def main():
+    client = authenticate("pyspread.json")
+    try:
+        workbook = client.open("test")
+        print("Test Sheet opened")
+    except:
+        workbook = client.create("test")
+        print("Test Sheet created")
+    sheet = workbook.sheet1
+    sheet.update("A1", [[1, 2], [3, 4]])
+    workbook.share("YOUR_EMAIL_ADDRESS", perm_type="user", role="writer")
+
+
+if __name__ == "__main__":
+    main()
