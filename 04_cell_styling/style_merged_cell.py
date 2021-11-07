ff90ae15e064bc3454e9f192be681731d97bdb3c
Mike Driscoll <mike@pythonlibrary.org>
2021-11-06 20:24:06 -0500

    Fix merged cell border bug

--- 04_cell_styling/style_merged_cell.py
+++ 04_cell_styling/style_merged_cell.py
@@ -7,7 +7,8 @@ from openpyxl.styles import Font, Border, Side, GradientFill, Alignment
 def merge_style(path):
     workbook = Workbook()
     sheet = workbook.active
-    sheet.merge_cells("A2:G4")
+    cell_range = "A2:G4"
+    sheet.merge_cells(cell_range)
     top_left_cell = sheet["A2"]
 
     light_purple = "00CC99FF"
@@ -16,8 +17,10 @@ def merge_style(path):
     double = Side(border_style="double", color=green)
 
     top_left_cell.value = "Hello from PyOpenXL"
-    top_left_cell.border = Border(top=double, left=thin, right=thin,
-                                  bottom=double)
+    for column in sheet[cell_range]:
+        for cell in column:
+            cell.border = Border(top=double, left=thin, right=thin,
+                                bottom=double)
     top_left_cell.fill = GradientFill(stop=("000000", "FFFFFF"))
     top_left_cell.font  = Font(b=True, color="FF0000", size=16)
     top_left_cell.alignment = Alignment(horizontal="center",
